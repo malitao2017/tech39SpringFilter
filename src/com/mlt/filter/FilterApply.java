eadffd1a9ec41ab007a96221fa4773e5501454a4
malitao2017 <934921721@qq.com>
2017-10-19 14:11:44 +0800

    增加监听器的内容

--- src/com/mlt/filter/FilterApply.java
+++ src/com/mlt/filter/FilterApply.java
@@ -8,9 +8,18 @@ import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
+
+//主要目的：过滤字符编码；其次，做一些应用逻辑判断等.
+// Filter跟web应用一起启动
+// 当web应用重新启动或销毁时，Filter也被销毁
 public class FilterApply implements Filter{
 
+	//私有配置文件
+	private FilterConfig config = null;
+	
 	@Override
 	public void destroy() {
         System.out.println("过滤器：destroy...");
@@ -20,12 +29,21 @@ public class FilterApply implements Filter{
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
 			throws IOException, ServletException {
 		System.out.println("过滤器：before...");
-        chain.doFilter(request, response);
+		//强制类型转化
+		HttpServletRequest req = (HttpServletRequest)request;
+		HttpServletResponse res = (HttpServletResponse)response;
+		// 获取web.xm设置的编码集，设置到Request、Response中
+		req.setCharacterEncoding(config.getInitParameter("charset"));
+		res.setContentType(config.getInitParameter("contentType"));
+		res.setCharacterEncoding(config.getInitParameter("charset"));
+		//将请求转发到目的地
+		chain.doFilter(request, response);
         System.out.println("过滤器：after...");
 	}
 
 	@Override
 	public void init(FilterConfig arg0) throws ServletException {
+		this.config = arg0;
         System.out.println("过滤器：init...");
 	}
 
--- src/com/mlt/web/ApplyController.java
+++ src/com/mlt/web/ApplyController.java
@@ -1,12 +1,20 @@
 package com.mlt.web;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.tomcat.dbcp.dbcp2.BasicDataSource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
 
 
 @Controller
@@ -20,8 +28,20 @@ public class ApplyController
 	}
 	
 	@RequestMapping(value="/only",method= RequestMethod.GET)
-	public String only(Model model,HttpServletRequest request,HttpServletResponse response){
-		System.out.println("调用了：/control/only ：request："+request.getAttribute("request"));
+	public String only(Model model,HttpServletRequest request,HttpServletResponse response) throws SQLException{
+		System.out.println("调用了：/control/only ");
+		System.out.println("拦截器中生成的数据：request："+request.getAttribute("request"));
+		System.out.println("监听器中添加的servletContext的数据：dataSource：");
+		ServletContext sc = request.getServletContext();
+		BasicDataSource bds = (BasicDataSource) sc.getAttribute("dataSource");
+		Connection con = bds.getConnection();
+		PreparedStatement pre = con.prepareStatement("select * from t_user where t_id=?");
+		pre.setInt(1, 1);
+		ResultSet rs = pre.executeQuery();
+		if(rs.next()){
+			String login = rs.getString("t_login_name");
+			System.out.println("id唯一的loginname为："+login);
+		}
 		return "welcome";
 	}
 }
